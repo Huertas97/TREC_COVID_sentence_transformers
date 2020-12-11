9d282c1666ba06a3459031138b693ffe5ea88556
Huertas97 <ahuertasg01@gmail.com>
2020-12-11 18:34:03 +0100

    Output and comments edited

--- scripts/ensemble_dim_red_cos_sim_trec_covid.py
+++ scripts/ensemble_dim_red_cos_sim_trec_covid.py
@@ -48,6 +48,8 @@ Usage:
 Options:
     -d, --data              Path to TREC-COVID parsed data
     -m, --model             Name of Transformer-based model from https://huggingface.co/pricing
+        -d, --data              Path to TREC-COVID parsed data
+    -m, --model             Name of Transformer-based model from https://huggingface.co/pricing
     -f, --fulltext          Bool: Include fulltext corpus for BM25 scoring
     -a, --abstract          Bool: Include abstract corpus for BM25 scoring  
     -t, --title             Bool: Include titles corpus for BM25 scoring  
@@ -55,7 +57,7 @@ Options:
 
 
 Example:
-    python cos_sim_trec_covid.py -b 1000 -t -a -f --data ./trec_covid_data/df_docs.pkl --model distiluse-base-multilingual-cased""")
+    python ensemble_dim_red_cos_sim_trec_covid.py -b 1000 -t -a --data ./trec_covid_data/df_docs.pkl --model distiluse-base-multilingual-cased,distilbert-multilingual-nli-stsb-quora-ranking""")
     sys.exit()
     
 if options.help or not options.data or not options.model:
@@ -99,9 +101,87 @@ nlp = en_core_sci_sm.load(disable=['ner', 'tagger'])
 nlp.max_length = 2000000
 
 # Sentence Transformer model
+class ensemble_stransformer:
+    
+  def __init__(self, model_names):
+    self.emb_dim = 0
+    # args is a list with a list of all models
+    
+    for i, arg in enumerate(model_names):
+      sentence_model = SentenceTransformer(arg)
+      self.emb_dim += sentence_model.get_sentence_embedding_dimension()
+      new_model_att = {"model_"+str(i): sentence_model}
+      self.__dict__.update(new_model_att)
+
+  
+  def encode(self, sentences, convert_to_tensor=True, show_progress_bar=False):
+    embeddings = []
+    for i, att in enumerate(dir(self)):
+      if "model" in att:
+        emb = getattr(self, att).encode(sentences, convert_to_tensor=convert_to_tensor, show_progress_bar=show_progress_bar)
+        
+
+        embeddings.append(emb)
+
+
+
+class ensemble_stransformer:
+    
+  def __init__(self, model_names, pca_list):
+    self.emb_dim = 0
+    
+    # Set models and PCAs as attributes
+    for i, (model_arg, pca_arg) in enumerate(zip(model_names, pca_list)):
+      sentence_model = SentenceTransformer(model_arg)
+      self.emb_dim += sentence_model.get_sentence_embedding_dimension()
+      new_model_att = {"model_"+str(i): sentence_model,
+                        "model_" + str(i) + "_pca": pca_arg}
+      self.__dict__.update(new_model_att)
+
+  def PCA(self, name, embedding):
+    for att in dir(self):
+      if name + "_pca" == att:
+        emb_dim_red = getattr(self, att).transform(embedding)
+        return emb_dim_red
+
+  def encode(self, sentences):
+    
+    embeddings = []
+    for att in dir(self):
+      if "model_" in att and "pca" not in att:
+        emb = getattr(self, att).encode(sentences)
+        emb_dim_red = self.PCA(att, emb)
+        embeddings.append(emb_dim_red)
+   
+    embeddings_concat =torch.cat(embeddings, dim=1) 
+    return embeddings_concat
+
+
+
+
+
 logger.info("-------- Loading SentenceTransformer model --------")
-embedder = SentenceTransformer(options.model)
-dim = embedder.get_sentence_embedding_dimension()
+# Create list of name models and added it to SentEval Engine parameters
+PATH_TO_PCA = './PCA'
+
+# Extract PCA fitted for the multilingual models
+df_pca_data = os.path.join(PATH_TO_PCA, options.data)
+df_ensemble_pca = pd.read_pickle(df_pca_data)
+models_names = df_ensemble_pca.Modelo.to_list()
+models_pca = df_ensemble_pca.PCA.to_list()
+
+# Selecting PCA from models introduced by command line
+model_names = options.model.split(",")
+pca_list = []
+for name in model_names:
+  pca = df_ensemble_pca[df_ensemble_pca["Modelo"] == name].PCA.values[0]
+  pca_list.append(pca)
+
+# Creating the models with PCA that will be used to obtain embeddings by SentEval
+embedder = ensemble_stransformer(model_names, pca_list)
+dim = embedder.emb_dim
+
+
 
 # # Corpus
 logger.info("-------- Building corpus --------")
@@ -151,9 +231,7 @@ corpus_score_batches = []
 # corpus_list = []
 for i in tqdm(range(0, len(corpus_list[0]), batch_size), desc = "Batches", position =0):
   corpus_batch_list = [c[i:i+batch_size] for c in corpus_list]
-  # ft_batch_c = fulltext_corpus[i:i+batch_size]
-  # abs_batch_c = abstract_corpus[i:i+batch_size]
-  # t_batch_c = title_corpus[i:i+batch_size]
+
 
 
 
@@ -161,21 +239,17 @@ for i in tqdm(range(0, len(corpus_list[0]), batch_size), desc = "Batches", posit
   # Score for each corpus
   ######################################################
   print("-------- Computing scores --------")
-  # name_corpus_list = ["fulltext", "abstract", "title"]
-  # corpus_list = [ft_batch_c, abs_batch_c, t_batch_c]
   corpus_score_batch = np.zeros( ( len(corpus_list), len(list(topics.keys())), len(corpus_batch_list[0]) ) )
 
   for corpus_idx, (name_corpus, corpus) in enumerate(zip(name_corpus_list, corpus_batch_list)): 
-    # Computing corpus embeddings
-    # print("-------- Computing {} embeddings --------".format(name_corpus))
-    # print("'\r{0}".format(idx), end='')
-    # print("'\r-------- Computing {} embeddings --------".format(name_corpus), end="")
+
     corpus_embeddings = torch.zeros(size = (len(corpus), dim), dtype=torch.float32)
     for i, t in enumerate( tqdm(corpus, total = len(corpus), desc = name_corpus + " embeddings", position = 0, leave=False) ):
       if t != "" or len(t) != 0:
         corpus_embeddings[i] = compute_abs_emb(t, embedder)
-      else:
-        corpus_embeddings[i] = embedder.encode(t, convert_to_tensor=True, show_progress_bar=False)
+      else:  
+        emb = embedder.encode([t], convert_to_tensor=True, show_progress_bar=False)
+        corpus_embeddings[i] = torch.flatten(emb, start_dim=0)
 
 
     
@@ -185,7 +259,6 @@ for i in tqdm(range(0, len(corpus_list[0]), batch_size), desc = "Batches", posit
     
     # Extracting text from each topic
     topics_score = np.zeros(  ( len(list(topics.keys())), len(corpus) )  )
-    # logger.info("-------- Computing BM25 and cosine similarity scores --------".format(name_corpus))
     for topic_idx, (n_topic, topic_data) in enumerate(tqdm(topics.items(), desc = "Topic", position =0, leave = False)):
       query = topic_data["query"]
       question = topic_data["question"]
@@ -193,18 +266,24 @@ for i in tqdm(range(0, len(corpus_list[0]), batch_size), desc = "Batches", posit
 
 
       # Corpus vs Topic Query scores
-      query_embedding = embedder.encode(query, convert_to_tensor=True, show_progress_bar=False)
+      query_embedding = embedder.encode([query], convert_to_tensor=True, show_progress_bar=False)
+      query_embedding =  torch.flatten(query_embedding, start_dim=0)
+      
       cos_scores = util.pytorch_cos_sim(query_embedding, corpus_embeddings)[0]
       query_cos_scores = cos_scores.cpu()
 
       # Corpus vs Topic Question scores
-      question_embedding = embedder.encode(question, convert_to_tensor=True, show_progress_bar=False)
+      question_embedding = embedder.encode([question], convert_to_tensor=True, show_progress_bar=False)
+      question_embedding =  torch.flatten(question_embedding, start_dim=0)
+      
       cos_scores = util.pytorch_cos_sim(question_embedding, corpus_embeddings)[0]
       question_cos_scores = cos_scores.cpu()
 
 
       # Corpus vs Topic Narrative scores
-      narrative_embedding = embedder.encode(narrative, convert_to_tensor=True, show_progress_bar=False)
+      narrative_embedding = embedder.encode([narrative], convert_to_tensor=True, show_progress_bar=False)
+      narrative_embedding =  torch.flatten(narrative_embedding, start_dim=0)
+      
       cos_scores = util.pytorch_cos_sim(narrative_embedding, corpus_embeddings)[0]
       narrative_cos_scores = cos_scores.cpu()
 
@@ -225,7 +304,7 @@ corpus_score = np.concatenate(corpus_score_batches, axis=2)
 logger.info("-------- Summation cosine similairty scores for all corpus --------")
 corpus_score_sum = np.sum(corpus_score,axis=0)
 
-model_name = options.model.split("/")[-1]
+model_name = options.model.replace("/", "_").replace(",", "_")
 
 logger.info("-------- Saving results in ./results/df_cos_sim_sc_{}.pkl --------".format(model_name))
 os.makedirs("./results", exist_ok=True)
